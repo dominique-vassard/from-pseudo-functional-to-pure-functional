cd984145c47a2764bea8afb71982f4bd46523056
Dominique VASSARD <dominique.vassard@gmail.com>
2018-04-09 21:02:36 +0200

    Counter in React

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -1,52 +1,82 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Grid, Col, Button, Row} from 'react-bootstrap';
 import './index.css';
 
 class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            current_count: 6
+        };
+    }
+
+    udpateCount(is_increment) {
+        const current_count = this.state.current_count;
+        if (is_increment) {
+            this.setState({current_count: current_count + 1});
+        } else {
+            this.setState({current_count: current_count - 1});
+        }
+    }
+
     renderCountDisplay(current_count) {
-        return <CountDisplay count={current_count} />;
+        return <CountDisplay count={current_count}/>;
     }
 
     render() {
         return (
-            <div >
-                <h1>This a simple counter</h1>
-                <Incrementer />
-                {this.renderCountDisplay(0)}
-                <Decrementer />
+            <div className="container jumbotron">
+                <Grid>
+                    <Row>
+                        <Col md={12} className="mx-auto">
+                            <center><h1>This a simple counter</h1></center>
+                        </Col>
+                    </Row>
+                    <Row className="mt-5" />
+                    <Row>
+                        <Col md={3}>
+                            <Decrementer  onClick={() => this.udpateCount(false)} />
+                        </Col>
+                        <Col md={6}>
+                            <center>
+                                <h1>{this.renderCountDisplay(this.state.current_count)}</h1>
+                            </center>
+                        </Col>
+                        <Col md={3}>
+                            <Incrementer  onClick={() => this.udpateCount(true)} />
+                        </Col>
+                    </Row>
+                </Grid>
             </div>
         );
     }
 }
 
-class Incrementer extends React.Component {
-    render() {
-        return (
-            <button className="count-updater" onClick={() => alert("+")}>
-                +
-            </button>
-        );
-    }
+function Incrementer(props) {
+    return (
+        <button className="btn btn-success btn-block" onClick={() => props.onClick()}>
+            +
+        </button>
+    );
 }
 
-class Decrementer extends React.Component {
-    render() {
-        return (
-            <button className="count-updater" onClick={() => alert("-")}>
-                -
-            </button>
-        );
-    }
+function Decrementer(props){
+    return (
+        <Button className="btn btn-danger btn-block" onClick={() => props.onClick()}>
+            -
+        </Button>
+    );
 }
 
-class CountDisplay extends React.Component {
-    render() {
-        return (
-            <div className="count-display">
-                <span>{this.props.count}</span>
-            </div>
-        );
-    }
+function CountDisplay(props) {
+    return (
+        <div className="count-display">
+            <span>{props.count}</span>
+        </div>
+    );
 }
 
 ReactDom.render(
