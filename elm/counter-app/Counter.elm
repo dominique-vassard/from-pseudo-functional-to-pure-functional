6dcca342a88670671ab520ca00cd2dce8935d2f0
Dominique VASSARD <dominique.vassard@kwanko.com>
2018-05-04 18:30:04 +0200

    Elm: counter-app OK

--- elm/counter-app/Counter.elm
+++ elm/counter-app/Counter.elm
@@ -4,10 +4,17 @@
 
 module Main exposing (..)
 
-import Html exposing (Html, program, div, button, text)
+import Html exposing (Html, program, div, button, text, h1)
+import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
+import Bootstrap.CDN as CDN
+import Bootstrap.Grid as Grid
+import Bootstrap.Grid.Col as Col
+import Bootstrap.Grid.Row as Row
+import Bootstrap.Button as Button
 
 
+main : Program Never Model Msg
 main =
     Html.program { init = init, view = view, update = update, subscriptions = subscriptions }
 
@@ -59,8 +66,34 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (toString model.count) ]
-        , button [ onClick Increment ] [ text "+" ]
+    Grid.container []
+        -- Responsive fixed width container
+        [ CDN.stylesheet
+          -- Interactive and responsive menu
+        , mainContent model
+        ]
+
+
+mainContent : Model -> Html Msg
+mainContent model =
+    div [ class "container jumbotron" ]
+        [ Grid.row []
+            [ Grid.col [ Col.md12 ]
+                [ h1 [ class "text-center" ]
+                    [ text "This is a simple counter"
+                    ]
+                ]
+            ]
+        , Grid.row [ Row.attrs [ class "mt-5" ] ] []
+        , Grid.row []
+            [ Grid.col [ Col.md3, Col.attrs [ class "text-center" ] ]
+                [ Button.button [ Button.danger, Button.attrs [ onClick Decrement ] ] [ text "-" ]
+                ]
+            , Grid.col [ Col.md6 ]
+                [ h1 [ class "text-center" ] [ text <| toString model.count ]
+                ]
+            , Grid.col [ Col.md3, Col.attrs [ class "text-center" ] ]
+                [ Button.button [ Button.success, Button.attrs [ onClick Increment ] ] [ text "+" ]
+                ]
+            ]
         ]
